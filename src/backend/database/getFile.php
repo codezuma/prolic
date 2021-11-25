82bf8104d4db525355ca8f3385aa3b8a6d07d630
chandresh patidar <patidarchandresh2002@gmail.com>
2021-11-25 10:05:09 +0530

    filename issue solved

--- src/backend/database/getFile.php
+++ src/backend/database/getFile.php
@@ -2,20 +2,17 @@
 $filepath = $_POST['filepath'];
 $filepath = "../../../".$filepath;
 $filename = basename($filepath);
-
-$output;
+$output['filename'] = $filename;
 if(file_exists($filepath)){
     header("Cache-Control: public");
     header("Content-Description: File Transfer");
     header("Content-Desposition: attachment; filename = $filename");
     header("Content-Type:application/");
     header("Content-Tranfer-Encoding:binary");
-
     readfile($filepath);
     exit;
 }
 else{
    $output['error'] = "file not found";
-   echo $output;
 }
 ?>
--- src/js/modules/components.js
+++ src/js/modules/components.js
@@ -1,21 +1,27 @@
 class fileComponent{
     constructor(fileElement){
-        console.log(fileElement);
         this.path = fileElement.getAttribute('data-filepath');
+        this.name = this.getFileName();
     }
-    open(){
+    getFileName(){
+        const pathSplitArray = this.path.split("/");
+        return  pathSplitArray.pop();
+    }
+    async get(){
       const fileObject = new FormData();
-      fileObject.append("filepath",this.path);  
-      fetch("../backend/database/getFile.php",{
-           method: 'POST', 
-           body: fileObject // body data type must match "Content-Type" header
+      fileObject.append("filepath",this.path); 
+       fetch("../backend/database/getFile.php",{
+            method: 'POST', 
+            body: fileObject // body data type must match "Content-Type" header
             }).then(response=>{response.blob().then(blob=>{
                 const data = window.URL.createObjectURL(blob);
-                                let ele = document.createElement('a');
-                ele.href = data;
-                ele.setAttribute("download","")
+                console.log(data);
+                let ele = document.createElement('a');
+                ele.href = data;    
+                ele.setAttribute("download",this.name)
                 ele.click();
-            })  });
+            })
+      });
     }
 }
 export{fileComponent};
